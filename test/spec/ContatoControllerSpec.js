5da36c5b8c2351d99d9fec38cc888b571148fac0
millys <millysfabrielle@gmail.com>
2016-05-13 16:44:27 -0300

    base

--- test/spec/ContatoControllerSpec.js
+++ test/spec/ContatoControllerSpec.js
@@ -2,20 +2,34 @@ describe('ContatoController', function(){
 
     var ctrl,
         $scope,
-        ContatoService;
+        ContatoService,
+        server;
+
+    var data = [
+        {_id: 1, nome: 'fulano 1', email: 'email1@teste.com'},
+        {_id: 2, nome: 'fulano 2', email: 'email2@teste.com', emergencia: 1},
+        {_id: 3, nome: 'fulano 3', email: 'email3@teste.com'}
+    ];
 
     beforeEach( function () {
         module('studying-node');
+        server = sinon.fakeServer.create();
         inject(function ($controller, $injector, $q) {
             $scope = $injector.get('$rootScope').$new();
 
             $controller('ContatoListCtrl', {
                 $scope: $scope,
                 ContatoService:{
-                    findAll: sinon.stub().returns( $q.when([{_id: 1, nome: 'fulano 1', email: 'email1@teste.com'}])),
+                    findAll: sinon.stub().returns( $q.when(data)),
                     findById: sinon.stub(),
                     save: sinon.stub(),
-                    delete: sinon.stub(),
+                    delete: sinon.stub()
+                        .withArgs({_id: 3, nome: 'fulano 3', email: 'email3@teste.com'}).returns(
+                            $q.when([
+                                {_id: 1, nome: 'fulano 1', email: 'email1@teste.com'},
+                                {_id: 2, nome: 'fulano 2', email: 'email2@teste.com', emergencia: 1}
+                            ])
+                        )
                 }
             });
             $scope.$digest();
@@ -23,8 +37,30 @@ describe('ContatoController', function(){
         });
     });
 
+    afterEach(function () {
+        server.restore();
+    });
+
     it('ContatoService findAll', function () {
-        expect($scope.contatos.length).to.equal(1);
+
+        server.respondWith("GET", "/contatos/list",
+            [
+                200,
+                { "Content-Type": "application/json" },
+                '{_id: 1, nome: "fulano 1", email: "email1@teste.com"}' +
+                '{_id: 2, nome: "fulano 2", email: "email2@teste.com"}'
+            ]);
+
+        expect($scope.contatos.length).to.equal(3);
+    });
+
+    it('ContatoService delete', function () {
+
+
+
+        var mockedContato = {_id: 33, nome: 'fulano 3', email: 'email3@teste.com'};
+        $scope.removerContato(mockedContato);
+        console.log($scope.contatos);
     });
 
 });
